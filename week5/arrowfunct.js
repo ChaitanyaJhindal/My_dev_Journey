a3d5e7824ee22612dcbbc1fa83ef9b883ac28c9b
Chaitanya Jhindal <Chaitanyavedansh@gmail.com>
2026-01-05 03:12:29 +0530

    understood the concpet of map, filter function for arrays

---
+++ week5/arrowfunct.js
@@ -0,0 +1,42 @@
+// function sum(a,b){
+//     return a+b;
+// }
+// const sum_arrow = (a,b) =>{
+//     return a+b;
+// }
+// const ans= sum(1,2);
+// const ans_arrow=sum_arrow(1,2);
+// console.log(ans);
+// console.log(ans_arrow);
+
+
+// Now coming towards the map and tranformation function 
+// const input =[1,2,3,4,5];
+// function transform(i){
+//     return i*2;
+// }
+// const ans = input.map(transform);
+// console.log(ans);
+
+
+
+
+// now coming to filtering 
+
+
+
+
+
+// const arr = [1,2,3,4,5];
+// const newarr=[];
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%2==0){
+//         newarr.push(arr[i]);
+//     }
+// }
+
+// console.log(newarr);
+
+
+// not a really cleaner wway but a classical way for the same now coming on js cool part 
+
