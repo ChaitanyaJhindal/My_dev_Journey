879dada47c23c6a848ab0486cc8033b0161810ce
Chaitanya Jhindal <Chaitanyavedansh@gmail.com>
2026-01-21 18:03:10 +0530

    Added admin route

---
+++ week8/routes/admin.js
@@ -0,0 +1,36 @@
+const {Router} = require("express")
+const adminRouter = router();
+
+adminRouter.post("/signup", function(req,res){
+    res.json({
+        message: "Signup endpoint"
+    })
+})
+
+adminRouter.post("/signin",function(req,res){
+    res.json({
+        message: "signin endpoint"
+    })
+})
+
+adminRouter.post("/course",function(req,res){
+    res.json({
+        message: "courses endpoint"
+    })
+})
+
+adminRouter.put("/course",function(req,res){
+    res.json({
+        message: "courses endpoint"
+    })
+})
+
+adminRouter.get("/course",function(req,res){
+    res.json({
+        message: "courses endpoint"
+    })
+})
+
+module.exports{
+    adminRouter
+};
