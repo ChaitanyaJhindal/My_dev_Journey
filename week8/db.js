3abf81df72c7be980e94690fcbd7471fd5335c99
Chaitanya Jhindal <Chaitanyavedansh@gmail.com>
2026-01-23 01:33:56 +0530

    Fixed errors while connecting

--- week8/db.js
+++ week8/db.js
@@ -1,23 +1,58 @@
-const {Schema} = require("mongoose");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const ObjectId = mongoose.Types.ObjectId;
 
-const userSchema = Schema({
+async function connectDB() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://chaitanyavedansh_db_user:chaitanya123@cluster0.gdjxqnz.mongodb.net/coursesellingapp"
+    );
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.log("❌ Not connected to MongoDB");
+    console.error(error);
+  }
+}
 
+connectDB();
+
+
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
 });
 
-
-const adminSchema = Schema({
-
+const adminSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
 });
 
-const courseSchema = Schema({
-
+const courseSchema = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageUrl: String,
+  creatorId: ObjectId,
 });
 
-const purchaseSchema = Schema({
-
+const purchaseSchema = new Schema({
+  userId: ObjectId,
+  courseId: ObjectId,
 });
 
-const userModel = mongoose.Model("user",userSchema);
-const adminModel = mongoose.Model("user",adminSchemaSchema);
-const courseModel = mongoose.Model("user",courseSchemaSchema);
-const purchaseModel = mongoose.Model("user",purchaseSchemaSchema);
+const userModel = mongoose.model("User", userSchema);
+const adminModel = mongoose.model("Admin", adminSchema);
+const courseModel = mongoose.model("Course", courseSchema);
+const purchaseModel = mongoose.model("Purchase", purchaseSchema);
+
+module.exports = {
+  userModel,
+  adminModel,
+  courseModel,
+  purchaseModel,
+};
