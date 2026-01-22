2a00d1f6459af401cd985d7ca7195f30e20364e7
Chaitanya Jhindal <Chaitanyavedansh@gmail.com>
2026-01-23 01:47:03 +0530

    Added dotenv file for the connection url

--- week8/index.js
+++ week8/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { UserRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -9,17 +10,17 @@ app.use(express.json());
 app.use("/user", UserRouter);
 app.use("/course", courseRouter);
 
+const uri = process.env.MONGODB_URI;
+const port = process.env.PORT || 3000;
+
 async function connectDB() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://chaitanyavedansh_db_user:chaitanya123@cluster0.gdjxqnz.mongodb.net/coursesellingapp"
-    );
+    await mongoose.connect(uri);
     console.log("✅ Connected to MongoDB");
 
-    app.listen(3000, () => {
-      console.log("🚀 Server running on port 3000");
+    app.listen(port, () => {
+      console.log(`🚀 Server running on port ${port}`);
     });
-
   } catch (error) {
     console.log("❌ Not connected to MongoDB");
     console.error(error);
