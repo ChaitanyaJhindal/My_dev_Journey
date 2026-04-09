eb65cd5cbec2d44021c89eaf7096664a44e1ba72
Chaitanya Jhindal <Chaitanyavedansh@gmail.com>
2026-04-10 01:29:38 +0530

    Corrected the code by resolving reference error

--- week11/customhooks/recoil/Recoil/src/App.jsx
+++ week11/customhooks/recoil/Recoil/src/App.jsx
@@ -1,47 +1,55 @@
 import './App.css'
-import { useState } from 'react';
-import {RecoilRoot,atom, useRecoilValue, useSetRecoilState} from 'recoil';
+import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from 'recoil';
+
+const counterAtom = atom({
+  key: 'counterAtom',
+  default: 0
+});
+
 function App() {
-return(
+  return (
     <RecoilRoot>
-  <Counter/>
+      <Counter />
     </RecoilRoot>
-  )
+  );
 }
-function Counter(){
 
-   return <div>
-    <CurrentCount />
-    <Increase/>
-    <Decrease/>
+function Counter() {
+  return (
+    <div>
+      <CurrentCount />
+      <Increase />
+      <Decrease />
     </div>
+  );
 }
-function CurrentCount(){
-  const count = useRecoilValue(counterAtom);
-  return <div>
-    {count}
-  </div>
-}
-function Decrease(){
-  const setCount = useSetRecoilState(count);
-  function decrease(){
-    setCount(c => c-1);
-  }
-  return <div>
-    <button onClick={decrease}>Decrease</button>
-  </div>
-}
-function Increase(){
-    const setCount = useSetRecoilState(count);
-  function increase(){
-    setCount(c => c+1);
-  }
-    return <div>
-    <button onClick = {increase}>Increase</button>
-  </div>
-}
-export default App;
 
+function CurrentCount() {
+  const count = useRecoilValue(counterAtom);
+  return <div>{count}</div>;
+}
+
+function Increase() {
+  const setCount = useSetRecoilState(counterAtom);
+
+  return (
+    <button onClick={() => setCount(c => c + 1)}>
+      Increase
+    </button>
+  );
+}
+
+function Decrease() {
+  const setCount = useSetRecoilState(counterAtom);
+
+  return (
+    <button onClick={() => setCount(c => c - 1)}>
+      Decrease
+    </button>
+  );
+}
+
+export default App;
 
 
 
