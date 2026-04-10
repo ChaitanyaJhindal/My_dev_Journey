41b805830213528fd0ca07d35fd18001dfc0ec65
Chaitanya Jhindal <Chaitanyavedansh@gmail.com>
2026-04-10 23:53:50 +0530

    Learning TS

--- week11/customhooks/recoil/Recoil/src/App.jsx
+++ week11/customhooks/recoil/Recoil/src/App.jsx
@@ -1,55 +1,180 @@
-import './App.css'
-import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from 'recoil';
-
-const counterAtom = atom({
-  key: 'counterAtom',
-  default: 0
-});
+import "./App.css";
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { counterAtom, evenSelector } from "./store/atoms/counter";
 
 function App() {
   return (
     <RecoilRoot>
-      <Counter />
+      <div
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "16px",
+          color: "#ffffff",
+        }}
+      >
+        <h1 style={{ margin: 0, fontSize: "32px" }}>Recoil Counter</h1>
+        <Counter />
+        <IsEven />
+        <Buttons />
+      </div>
     </RecoilRoot>
   );
 }
 
-function Counter() {
+function Buttons() {
+  const setCount = useSetRecoilState(counterAtom);
+
+  function increase() {
+    setCount((c) => c + 2);
+  }
+
+  function decrease() {
+    setCount((c) => c - 1);
+  }
+
   return (
-    <div>
-      <CurrentCount />
-      <Increase />
-      <Decrease />
+    <div style={{ display: "flex", gap: "12px" }}>
+      <button
+        onClick={increase}
+        style={{ padding: "10px 16px", fontSize: "16px", cursor: "pointer" }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={decrease}
+        style={{ padding: "10px 16px", fontSize: "16px", cursor: "pointer" }}
+      >
+        Decrease
+      </button>
     </div>
   );
 }
 
-function CurrentCount() {
+function Counter() {
   const count = useRecoilValue(counterAtom);
-  return <div>{count}</div>;
+  return <div style={{ fontSize: "28px", fontWeight: 700 }}>Count: {count}</div>;
 }
 
-function Increase() {
-  const setCount = useSetRecoilState(counterAtom);
-
-  return (
-    <button onClick={() => setCount(c => c + 1)}>
-      Increase
-    </button>
-  );
-}
-
-function Decrease() {
-  const setCount = useSetRecoilState(counterAtom);
-
-  return (
-    <button onClick={() => setCount(c => c - 1)}>
-      Decrease
-    </button>
-  );
+function IsEven() {
+  const even = useRecoilValue(evenSelector);
+  return <div>{even ? "Even" : "Odd"}</div>;
 }
 
 export default App;
+
+
+
+
+
+
+
+
+//C:\Users\Chait\Desktop\Cohort3\week11\customhooks\recoil\Recoil\src\App.jsx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css'
+// import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from 'recoil';
+
+// const counterAtom = atom({
+//   key: 'counterAtom',
+//   default: 0
+// });
+
+// function App() {
+//   return (
+//     <RecoilRoot>
+//       <Counter />
+//     </RecoilRoot>
+//   );
+// }
+
+// function Counter() {
+//   return (
+//     <div>
+//       <CurrentCount />
+//       <Increase />
+//       <Decrease />
+//     </div>
+//   );
+// }
+
+// function CurrentCount() {
+//   const count = useRecoilValue(counterAtom);
+//   return <div>{count}</div>;
+// }
+
+// function Increase() {
+//   const setCount = useSetRecoilState(counterAtom);
+
+//   return (
+//     <button onClick={() => setCount(c => c + 1)}>
+//       Increase
+//     </button>
+//   );
+// }
+
+// function Decrease() {
+//   const setCount = useSetRecoilState(counterAtom);
+
+//   return (
+//     <button onClick={() => setCount(c => c - 1)}>
+//       Decrease
+//     </button>
+//   );
+// }
+
+// export default App;
 
 
 
---
+++ week14/index.ts
@@ -0,0 +1,120 @@
+let greet = () =>{
+    console.log("Hi ! there ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function delayedcall(fun: ()=>void){
+//     setTimeout(fun,100);
+// }
+// delayedcall(function(){
+//     console.log("Hello world");
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function greet(num1: number  , num2: number ) {
+//     console.log(num1+num2);
+// }
+
+// greet(3,5);
