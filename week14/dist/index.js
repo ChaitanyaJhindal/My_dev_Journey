1b40d19120d682b1795c109e78d636e536e75001
Chaitanya Jhindal <Chaitanyavedansh@gmail.com>
2026-04-12 16:13:33 +0530

    Added interfaces , intefaces of intefaces and optional field as well

---
+++ week14/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let greet = () => {
+    console.log("Hi ! there ");
+};
+// function delayedcall(fun: ()=>void){
+//     setTimeout(fun,100);
+// }
+// delayedcall(function(){
+//     console.log("Hello world");
+// })
+// function greet(num1: number  , num2: number ) {
+//     console.log(num1+num2);
+// }
+// greet(3,5);
+//# sourceMappingURL=index.js.map
--- week14/index.ts
+++ week14/index.ts
@@ -1,6 +1,153 @@
-let greet = () =>{
-    console.log("Hi ! there ");
+interface Address {
+    city: String ;
+    country : String;
+    pincode: number;
+    houseNumber: String 
 }
+
+
+
+interface User {
+    name: string , 
+    age: number , 
+    address: Address
+}
+
+interface Office {
+    address : Address
+}
+let user2: User = {
+    name: "chaitanya",
+    age: 21,
+    address: {
+        city: "Fzd",
+        country: "India",
+        pincode: 123456,
+        houseNumber: "12A"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum(a: number , b: number): number {
+//     return a + b ;
+// } 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let greet = () =>{
+//     console.log("Hi ! there ");
+// }
 
 
 
